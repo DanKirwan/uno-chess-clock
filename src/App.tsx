04ed9c4a091bb4e1e28f6c4a41a98b133d952009
Dan Kirwan <dan.kirwan20@imperial.ac.uk>
2025-01-27 21:08:50 +0000

    added an undo button

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import useSound from 'use-sound';
 import next from './next.m4a';
 import lose from './lose.m4a';
 import flip from './wabada.m4a';
+import resetNoise from './gdonk.m4a';
 
 
 const MS_PER_SECOND = 1000;
@@ -20,15 +21,21 @@ function App() {
   const [playNext] = useSound(next);
   const [playFlip] = useSound(flip);
   const [playLose] = useSound(lose);
+  const [playReset] = useSound(resetNoise);
 
   const [playing, setPlaying] = useState(false);
-  const [increment, setIncrement] = useState(5 * MS_PER_SECOND);
-  const [startTime, setStartTime] = useState(60 * MS_PER_SECOND);
-  const [playerCount, setPlayerCount] = useState(5)
+  const [increment, setIncrement] = useState(2 * MS_PER_SECOND);
+  const [startTime, setStartTime] = useState(30 * MS_PER_SECOND);
+  const [playerCount, setPlayerCount] = useState(4)
 
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState<number>(0);
   const [names, setNames] = useState<string[]>([])
   const [times, setTimes] = useState<number[]>([])
+
+  const [prevPlayerIndex, setPrevPlayerIndex] = useState(0);
+  const [prevTimes, setPrevTimes] = useState<number[]>([]);
+  const [prevMultiplier, setPrevMultiplier] = useState(1);
+
   const reset = () => {
     setCurrentPlayerIndex(0);
     setTimes(range(0, playerCount).map(_ => startTime));
@@ -50,14 +57,16 @@ function App() {
 
   const [multiplier, setMultiplier] = useState(1);
 
-  const doNext = (multiplier: number) => {
+  const doNext = (prevMultiplier: number, multiplier: number) => {
     if (times[currentPlayerIndex] > 0) setTimes(c => c.map((x, i) => i === currentPlayerIndex ? x + increment : x))
+    setPrevPlayerIndex(currentPlayerIndex);
+    setPrevTimes(times);
+    setPrevMultiplier(prevMultiplier);
     setCurrentPlayerIndex(x => {
       let index = x;
       const validIndices = times.map((t, i) => [t, i]).filter(([t, i]) => t > 0).map(([t, i]) => i);
       if (validIndices.length === 0) return x;
       index = (index + multiplier + times.length) % times.length
-      console.log((index + multiplier + times.length) % times.length);
 
       while (!validIndices.includes(index)) {
         index = (index + multiplier + times.length) % times.length
@@ -68,15 +77,27 @@ function App() {
   useKey('Space', (e) => {
     if (!playing) return;
     playNext();
-    doNext(multiplier);
+    doNext(multiplier, multiplier);
   });
 
   useKey('Enter', (e) => {
     if (!playing) return;
     playFlip();
-    doNext(-multiplier);
+    doNext(multiplier, -multiplier);
     setMultiplier(m => -m);
   });
+
+
+
+  useKey('Escape', (e) => {
+
+    if (!playing) return;
+    playReset();
+    setTimes(prevTimes);
+    setCurrentPlayerIndex(prevPlayerIndex);
+    setMultiplier(prevMultiplier)
+  });
+
 
 
 
